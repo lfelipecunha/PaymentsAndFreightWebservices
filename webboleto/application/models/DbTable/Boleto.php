4e5938944b9b1af2ba207a4e187b7fa97bba56cc
luiz <luiz@96ac18a5-289d-4a71-9acf-8e1894fbe77c>
2011-11-01 13:00:35 +0000

    Alterações nos nomes das tabelas pois o armazenamento no banco de dados do servidor será junto com os os outros webservices, conforme solicitado pelo gerente de projetos.

--- webboleto/application/models/DbTable/Boleto.php
+++ webboleto/application/models/DbTable/Boleto.php
@@ -1,7 +1,7 @@
 <?php
 class Application_Model_DbTable_Boleto extends Zend_Db_Table_Abstract
 {
-	protected $_name = 'boleto';
+	protected $_name = 'webboleto_boleto';
 
 	protected $_primary = array('codigo');
 
@@ -9,14 +9,14 @@ class Application_Model_DbTable_Boleto extends Zend_Db_Table_Abstract
 		$db = $this->getAdapter();
 		$params = $db
 			->select()
-			->from(array('b' => 'boleto'),array())
+			->from(array('b' => 'webboleto_boleto'),array())
 			->where("b.codigo = '$codigo_boleto'");
 		if ($statics) {
 			$params->where('p.tipo_parametro = \'ESTATICO\'');
 		}
 		$params
-			->joinLeft(array('bhp' => 'boleto_has_parametro'),'b.codigo = bhp.codigo',array())
-			->joinLeft(array('p' => 'parametro'),'p.id_parametro = bhp.id_parametro',array('nome_parametro'));
+			->joinLeft(array('bhp' => 'webboleto_boleto_has_parametro'),'b.codigo = bhp.codigo',array())
+			->joinLeft(array('p' => 'webboleto_parametro'),'p.id_parametro = bhp.id_parametro',array('nome_parametro'));
 		return $db->fetchCol($params);
 	}
 
