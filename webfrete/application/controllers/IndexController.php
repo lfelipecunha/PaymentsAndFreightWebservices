30b78c3f3c0ee69119c4c6180dd923575c8debf5
luiz <luiz@96ac18a5-289d-4a71-9acf-8e1894fbe77c>
2011-11-09 18:16:25 +0000

    Remoção de parâmetros que eram utilizados para teste. Também realizada a inserção de comentários para facilitar o entendimento do código.

--- webfrete/application/controllers/IndexController.php
+++ webfrete/application/controllers/IndexController.php
@@ -3,119 +3,84 @@
 class IndexController extends Zend_Controller_Action
 {
 
-	public function init()
-	{
-		/* Initialize action controller here */
-	}
-
+	/**
+	 * Ação responsável pela realização de consultas de valor e prazo de fretes
+	 */
 	public function indexAction()
 	{
-		$params = array(
-			'cep_origem'     => '02380000',
-			'cep_destino'    => '93022000',
-			'jadlog_senha'   => 'F2s0C1i1',
-			'correios_login' => '',
-			'correios_senha' => '',
-			'valor_produtos' => '10000',
-			'cnpj'           => '09091523000165',
-			'produtos'       => array(
-				0 => array(
-					'altura'      => 10,
-					'largura'     => 50,
-					'comprimento' => 90,
-					'peso'        => 20000,
-					'quantidade'  => 10,
-				),
-				1 => array(
-					'altura'      => 60,
-					'largura'     => 20,
-					'comprimento' => 30,
-					'peso'        => 1000,
-					'quantidade'  => 10,
-				),
-				2 => array(
-					'altura'      => 10,
-					'largura'     => 10,
-					'comprimento' => 30,
-					'peso'        => 1000,
-					'quantidade'  => 6,
-				),
-			),
-			'opcoes'         => array(
-				0 => array(
-					'codigo' => 1,
-					'seguro' => 'N',
-					'valor_coleta' => 0,
-					'pagar_destino' => 'N',
-					'tipo_entrega'  => 'D',
-				),
-				1 => array(
-					'codigo' => 3,
-					'formato' => 1,
-					'servico_adicional' => 'N',
-					'valor_servico_adicional' => '0',
-					'aviso_recebimento' => 'N',
-				),
-				2 => array(
-					'codigo' => 2,
-					'seguro' => 'N',
-					'valor_coleta' => 0,
-					'pagar_destino' => 'N',
-					'tipo_entrega'  => 'D',
-				),
-			)
-		);
-
-
-
-
-//		if ($this->getRequest()->isPost) {
-//			$params = $this->getRequest()->getPost();
-	 		$validate = new Application_Model_Validate($params);
+		// verifica se o houve envio de dados por POST
+		if ($this->getRequest()->isPost) { // se houve envio
+			// pega os dados do POST
+			$params = $this->getRequest()->getPost();
+			// instância do Modelo de validação de dados padrões
+			$validate = new Application_Model_Validate($params);
+			// pega os dados validados. Neste ponto podem ser lançadas exceções
 			$params = $validate->validaParams();
 
+			// instância do objeto de Tabela de Tipo de Frete
 			$tipo_frete = new Application_Model_DbTable_TipoFrete();
-
+			// inicialização de array dos tipos de fretes
 			$fretes = array();
+			// laço para obtenção dos valores e prazos de frete para cada opcao
+			// passada por parâmetro
 			foreach ($params['opcoes'] as $opcao) {
+				// pega os dados do tipo de frete de acordo com o código do tipo
 				$dados = $tipo_frete->getById($opcao['codigo']);
+				// grupo do tipo de frete. Utilizado para definir qual classe de 
+				// modelo será utilizada
 				$grupo = $dados[0]['nome_grupo'];
+
+				// nome do tipo de frete. Utilizado no retorno dos dados
 				$nome = $dados[0]['nome_tipo_frete'];
+
 				try {
+					// instância de filtro para montagem do nome da classe
 					$filtro = new Zend_Filter_Word_UnderscoreToCamelCase();
-					$nome_classe = $filtro->filter($grupo);
-					$reflection = new Zend_Reflection_Class('Application_Model_'.$nome_classe);
-					$opcao['codigo_tipo_frete'] = $dados[0]['codigo_tipo_frete'];
-					$model = $reflection->newInstance($params,$opcao);
+					// nome da classe de modelo para obtenção dos dados
+					$nome_classe = 'Application_Model_'.$filtro->filter($grupo);
+					// instância do modelo de acordo com o grupo do tipo de 
+					// frete. Para obtenção de dados
+					$model = new $nome_classe($params,$opcao);
+					// armazena os dados do frete no array de fretes
 					$fretes[$grupo][$nome] = $model->consulta();
-				} catch (Zend_Exception_Frete $e) {
+
+				} catch (Zend_Exception_Frete $e) { 
+					// Se houve um erro que é essencial para o sistema, o mesmo
+					// é lançado novamente para que seja tratado pelo mecanismo
+					// responsável. Vide documentação sobre tratamento de erros
+					// no arquivo TratamentoErros.txt
 					throw $e;
 				} catch (Exception $e) {
+					// Se houve uma exceção qualquer pega o código
 					$fretes[$grupo][$nome]['erro'] = $e->getCode();
 				}
 			}
 
+			// Criação de objeto de XML para geração do retorno
 			$xml = new SimpleXMLElement('<?xml version="1.0" encoding="UTF-8"?><consulta></consulta>');
+			// laço para inserção dos tipos de frete no xml
 			foreach ($fretes as $tipos) {
+				// laço para cada tipo de frete
 				foreach ($tipos as $nome => $valores) {
+					// adiciona um Filho para o Xml. nome do tipo de frete
 					$tipo = $xml->addChild(strtolower($nome));
+					// adiciona um filho para o tipo de frete com o nome do tipo
 					$tipo->addChild('nome',$nome);
+					// laço para acessar os dados do tipo de frete
 					foreach ($valores as $key => $value) {
+						// adiciona um Filho para o tipo de frete com o dado
 						$tipo->addChild($key, $value);
 					}
 				}
 			}
+			// seta o Cabeçalho da página para renderização de XML
 			$this->getResponse()->setHeader('Content-type','text/xml');
+			// envia o XML para camada de visualização
 			$this->view->xml = $xml;
-//		}else {
-//			throw new Zend_Exception_Frete('O acesso à este serviço é somente por POST!');
-//		}
+		}else { // se o accesso ao serviço não foi por POST
+			// lança erro
+			throw new Zend_Exception_Frete('O acesso à este serviço é somente por POST!');
+		}
 	}
-
-	public static function teste() {
-		throw new ErrorException('oi');
-	}
-
-
 }
 
