081aefc55912230507099b028555ad87f8a6969c
luiz <luiz@96ac18a5-289d-4a71-9acf-8e1894fbe77c>
2011-12-01 12:55:06 +0000

    Adição de modelo para q a implementação da opção de frete retirar na loja

---
+++ webfrete/application/models/RetiraNaLoja.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Classe que cálcula o preço e o prazo para o frete quando o(s) produto(s) 
+ * for(em) retirado(s) na loja.
+ *
+ */
+class Application_Model_RetiraNaLoja implements Application_Model_Frete
+{
+	/**
+	 * Valor do frete em centavos
+	 * @var int
+	 */
+	protected $_valor = 0;
+
+	/**
+	 * Construtor da classe
+	 *
+	 * @param mixed $params Parâmetro obrigatório mas não utilizado
+	 * @param array $opcao  Parâmetro com o valor adicional do frete
+	 */
+	public function __construct($params,$opcao) {
+		// se o valor adicional estiver setado seta o valor do frete com este 
+		// valor
+		if (!empty($opcao['valor_servico_adicional'])) {
+			$this->_valor = $opcao['valor_servico_adicional'];
+		}
+	}
+
+	/**
+	 * Método para retorno do valor e prazo de entrega do frete
+	 *
+	 * @return array
+	 */
+	public function consulta() {
+		return array(
+			'prazo' => 0,
+			'valor' => $this->_valor,
+			'erro'  => 0,
+		);
+	}
+}
