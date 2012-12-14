31a8da751a78ac51376ff0140285718146c84a7f
luiz <luiz@96ac18a5-289d-4a71-9acf-8e1894fbe77c>
2012-12-14 13:43:54 +0000

    Correção para que não sejam necessárias alterações manuais no arquivo csv que contém a tabela de frete

--- webfrete/auxiliares/gerador_tabela_frete_vinhos/parser.php
+++ webfrete/auxiliares/gerador_tabela_frete_vinhos/parser.php
@@ -4,7 +4,7 @@ $lines = explode("\n",$file);
 $firstLine = array_shift($lines);
 $columns = explode("\t",$firstLine);
 foreach ($columns as &$column) {
-    $column = strtolower($column);
+    $column = str_replace(array(' ','kg'),'',strtolower(trim($column)));
 }
 $data = array();
 $pesos = array(30,50,100,150,200,250,300,350,400,410,450,470,500,520,550,600);
@@ -17,7 +17,7 @@ foreach ($lines as $region) {
     reset($columns);
     $aux = array();
     foreach ($infos as $info) {
-        $aux[current($columns)] = $info;
+        $aux[current($columns)] = trim($info);
         next($columns);
     }
     $key = $aux['regiao'];
@@ -29,7 +29,7 @@ foreach ($lines as $region) {
     );
     foreach ($pesos as $peso) {
         if (!empty($aux[$peso]) && $aux[$peso] != '-') {
-            $data[$key]['opcoes'][$peso] = (float)$aux[$peso];
+            $data[$key]['opcoes'][$peso] = (float)str_replace(array('R$',','),array('','.'),trim($aux[$peso]));
         }
     }
 }
