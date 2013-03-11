2215221a1e759c6e74fb4346ee2c928f76e41f6b
luiz <luiz@96ac18a5-289d-4a71-9acf-8e1894fbe77c>
2013-03-11 13:31:15 +0000

    Alteração de geração de mensagem de erro

--- bcash/Library/Validation/Validator/In.php
+++ bcash/Library/Validation/Validator/In.php
@@ -7,7 +7,15 @@ class Validation_Validator_In extends Validation_Validator_Abstract {
     public function isValid($value) {
         $isValid = true;
         if (!in_array($value,$this->getValues())) {
-            $this->_errorMessage = sprintf('O valor deve ser %s',implode(' ou ',$this->getValues()));
+            $values = $this->getValues();
+            if (count($values) > 2) {
+                $last = array_pop($values);
+                $values = implode(', ',$values);
+                $values .= ' ou '.$last;
+            } else {
+                $values = current($values);
+            }
+            $this->_errorMessage = sprintf('O valor deve ser %s',$values);
             $isValid = false;
         }
         return $isValid;
